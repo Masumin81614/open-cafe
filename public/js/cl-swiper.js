a303d191f1a48fe46fded004201aa88addb8d5b9
Masumin81614 <hlkcorporation@gmail.com>
2025-11-17 22:26:12 +0900

    Special Lunch Setのリボンまで完成

--- public/js/cl-swiper.js
+++ public/js/cl-swiper.js
@@ -1,13 +1,13 @@
 const swiper = new Swiper(".swiper", {
   loop: true,
-  effect: "fade", // フェード切り替え
-  // 自動再生
+  effect: "fade",
+
   autoplay: {
-    delay: 4000, // 4秒後に次のスライドへ
-    disableOnInteraction: false, // ユーザーが操作しても自動再生を継続
+    delay: 4000,
+    disableOnInteraction: false,
   },
   speed: 4000,
-  // ページネーション
+
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
